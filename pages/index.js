0f44202690bcb078216ef1ebe1a007731aed615b
Ceyda <ceydaonemli2@gmail.com>
2022-04-28 01:43:57 +0300

    tailwind integration and utility-first  bitirmek çalışması kanalına mesaj gönder.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
     <>
-      Hello World
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+        <div className="shrink-0">
+          <img className="h-12 w-12" src="/vercel.svg" alt="ChitChat Logo" />
+        </div>
+        <div>
+          <div className="text-xl font-medium text-black">ChitChat</div>
+          <p className="text-slate-500">You have a new message!</p>
+        </div>
+      </div>
     </>
-  )
+  );
 }
