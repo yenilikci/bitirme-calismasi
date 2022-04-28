f0e761ecd6d803a9a3db20e640fd0a4618f347f4
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-04-29 00:09:31 +0300

    init pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        "8xl": "1920px",
+      },
+    },
   },
   plugins: [],
-}
+};
