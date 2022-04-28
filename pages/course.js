fcf5fcff8afe210d34904abfe1c8436a676157b6
Ceyda <ceydaonemli2@gmail.com>
2022-04-29 00:22:45 +0300

    add base layout

--- pages/course.js
+++ pages/course.js
@@ -1,14 +1,19 @@
 import {CourseHero, Curriculum, Keypoints} from "@components/course";
 import {Modal} from "@components/common";
+import {BaseLayout} from "@components/layout";
 
 export default function Course() {
 
     return (
-        <div className="relative max-w-7xl mx-auto px-4">
-            <CourseHero/>
+        <BaseLayout>
+            <div className="py-4">
+                <CourseHero/>
+            </div>
             <Keypoints/>
-            <Curriculum/>
+            <div className="py-4">
+                <Curriculum/>
+            </div>
             <Modal/>
-        </div>
+        </BaseLayout>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,18 @@
-import {Breadcrumbs, Footer, Hero, Navbar} from "@components/common";
+import {Breadcrumbs, Hero} from "@components/common";
 import {EthRates, Walletbar} from "@components/web3";
 import {CourseList} from "@components/course";
 import {OrderCard} from "@components/order";
+import {BaseLayout} from "@components/layout";
 
 export default function Home() {
   return (
-    <div>
-      <div className="relative bg-white overflow-hidden">
-        <div className="relative max-w-7xl mx-auto px-4">
-          <Navbar/>
-          <div className="fit">
-            <Hero/>
-            <Breadcrumbs/>
-            <Walletbar/>
-            <EthRates/>
-            <OrderCard/>
-            <CourseList/>
-          </div>
-        </div>
-        <Footer />
-      </div>
-    </div>
+      <BaseLayout>
+          <Hero/>
+          <Breadcrumbs/>
+          <Walletbar/>
+          <EthRates/>
+          <OrderCard/>
+          <CourseList/>
+      </BaseLayout>
   );
 }
