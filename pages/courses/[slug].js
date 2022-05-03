add49436166af66bba05387a0a8cbd2072fdf0f3
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-05-03 23:37:19 +0300

    get single course, get static paths and props

--- pages/courses/[slug].js
+++ pages/courses/[slug].js
@@ -1,11 +1,13 @@
 import {CourseHero, Curriculum, Keypoints} from "@components/course";
 import {Modal} from "@components/common";
 import {BaseLayout} from "@components/layout";
+import {getAllCourses} from "@content/courses/fetcher";
 
-export default function Course() {
+export default function Course({course}) {
 
     return (
         <>
+            {course.title}
             <div className="py-4">
                 <CourseHero/>
             </div>
@@ -16,6 +18,30 @@ export default function Course() {
             <Modal/>
         </>
     );
+}
+
+export function getStaticPaths() {
+    const {data} = getAllCourses();
+
+    return {
+        paths: data.map(c => ({
+            params: {
+                slug: c.slug
+            }
+        })),
+        fallback: false
+    }
+}
+
+export function getStaticProps({params}) {
+    const {data} = getAllCourses();
+
+    const course = data.filter(c => c.slug == params.slug)[0];
+    return {
+        props: {
+            course
+        },
+    };
 }
 
 Course.Layout = BaseLayout;
