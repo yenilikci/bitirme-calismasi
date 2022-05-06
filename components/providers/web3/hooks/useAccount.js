dc02dcf521424c9e6f208be883927303f96614b0
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-05-06 23:57:20 +0300

    add wrapper enhance hook

--- components/providers/web3/hooks/useAccount.js
+++ components/providers/web3/hooks/useAccount.js
@@ -1,35 +1,33 @@
-import {useEffect} from "react";
-import useSWR from "swr";
+import {useEffect} from "react"
+import useSWR from "swr"
 
 const adminAddresses = {
     "0xcf9201674645e205581d5dc064f5f70902c47b93a32838a78129519820b3041f": true
 }
 
 export const handler = (web3, provider) => () => {
-    // const [account, setAccount] = useState(null);
+
     const {data, mutate, ...rest} = useSWR(() =>
             web3 ? "web3/accounts" : null,
         async () => {
-            const accounts = await web3.eth.getAccounts();
-            return accounts[0];
+            const accounts = await web3.eth.getAccounts()
+            return accounts[0]
         }
-    );
+    )
 
     useEffect(() => {
-        // window.ethereum
         provider &&
-        provider.on("accountsChanged", accounts => {
-            mutate(accounts[0] ?? null);
-        });
+        provider.on("accountsChanged",
+            accounts => mutate(accounts[0] ?? null)
+        )
     }, [provider])
 
-
     return {
-        account: {
-            data,
-            isAdmin: (data && adminAddresses[web3.utils.keccak256(data)]) ?? false,
-            mutate,
-            ...rest
-        }
+        data,
+        isAdmin: (
+            data &&
+            adminAddresses[web3.utils.keccak256(data)]) ?? false,
+        mutate,
+        ...rest
     }
-};
+}
--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -1,13 +1,13 @@
-import Link from 'next/link';
-import {useWeb3} from "@components/providers";
-import {Button} from "@components/ui/common";
-import {useAccount} from "@components/hooks/web3/useAccount";
-import {useRouter} from "next/router";
+import {useWeb3} from "@components/providers"
+import Link from "next/link"
+import {Button} from "@components/ui/common"
+import {useAccount} from "@components/hooks/web3"
+import {useRouter} from "next/router"
 
-export default function Navbar() {
-    const {connect, isLoading, isWeb3Loaded} = useWeb3();
-    const {account} = useAccount();
-    const {pathname} = useRouter();
+export default function Footer() {
+    const {connect, isLoading, isWeb3Loaded} = useWeb3()
+    const {account} = useAccount()
+    const {pathname} = useRouter()
 
     return (
         <section>
@@ -17,75 +17,64 @@ export default function Navbar() {
                         <div>
                             <Link href="/">
                                 <a
-                                    className="font-medium mr-8 text-gray-500 hover:text-gray-900"
-                                >
+                                    className="font-medium mr-8 text-gray-500 hover:text-gray-900">
                                     Home
                                 </a>
                             </Link>
                             <Link href="/marketplace">
                                 <a
-                                    className="font-medium mr-8 text-gray-500 hover:text-gray-900"
-                                >
+                                    className="font-medium mr-8 text-gray-500 hover:text-gray-900">
                                     Marketplace
                                 </a>
                             </Link>
-                            <Link href="/blogs">
+                            <Link href="/">
                                 <a
-                                    className="font-medium mr-8 text-gray-500 hover:text-gray-900"
-                                >
+                                    className="font-medium mr-8 text-gray-500 hover:text-gray-900">
                                     Blogs
                                 </a>
                             </Link>
-
                         </div>
                         <div>
-                            <Link href="/wishlist">
+                            <Link href="/">
                                 <a
-                                    className="font-medium mr-8 text-gray-500 hover:text-gray-900"
-                                >
+                                    className="font-medium mr-8 text-gray-500 hover:text-gray-900">
                                     Wishlist
                                 </a>
                             </Link>
-                            {
-                                isLoading ?
-                                    <Button
-                                        disabled={true}
-                                        onClick={connect}
-                                    >
-                                        Loading...
-                                    </Button> :
-                                    isWeb3Loaded ?
-                                        account.data ?
-                                            <Button
-                                                hoverable={false}
-                                                className="cursor-default"
-                                            >
-                                                Hi there {account.isAdmin && 'Admin'}
-                                            </Button> :
-                                            <Button
-                                                onClick={connect}
-                                            >
-                                                Connect Wallet
-                                            </Button> :
+                            {isLoading ?
+                                <Button
+                                    disabled={true}
+                                    onClick={connect}>
+                                    Loading...
+                                </Button> :
+                                isWeb3Loaded ?
+                                    account.data ?
                                         <Button
-                                            onClick={() =>
-                                                window.open("https://metamask.io/download.html", "_blank<")}
-                                            className="text-white bg-lime-600 hover:bg-lime-700"
-                                        >
-                                            Install Metamask
-                                        </Button>
+                                            hoverable={false}
+                                            className="cursor-default">
+                                            Hi there {account.isAdmin && "Admin"}
+                                        </Button> :
+                                        <Button
+                                            onClick={connect}>
+                                            Connect
+                                        </Button> :
+                                    <Button
+                                        onClick={() => window.open("https://metamask.io/download.html", "_blank")}>
+                                        Install Metamask
+                                    </Button>
                             }
                         </div>
                     </div>
                 </nav>
             </div>
-            {
-                account.data && !pathname.includes("/marketplace") && <div className="flex justify-end pt-1 sm:px-6 lg:px-8">
-                    <div className="text-white bg-indigo-600 rounded-md p-2">
-                        {account.data}
-                    </div>
+            {account.data &&
+            !pathname.includes("/marketplace") &&
+            <div className="flex justify-end pt-1 sm:px-6 lg:px-8">
+                <div className="text-white bg-indigo-600 rounded-md p-2">
+                    {account.data}
                 </div>
+            </div>
             }
         </section>
-    );
+    )
 }
