523bd6ad68c1af17e730bbb7458e400c4ec95b9d
Ceyda <ceydaonemli2@gmail.com>
2022-05-06 23:43:52 +0300

    hide address navbar on marketplace

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -2,11 +2,12 @@ import Link from 'next/link';
 import {useWeb3} from "@components/providers";
 import {Button} from "@components/ui/common";
 import {useAccount} from "@components/providers/hooks/web3/useAccount";
+import {useRouter} from "next/router";
 
 export default function Navbar() {
     const {connect, isLoading, isWeb3Loaded} = useWeb3();
     const {account} = useAccount();
-
+    const {pathname} = useRouter();
 
     return (
         <section>
@@ -79,7 +80,7 @@ export default function Navbar() {
                 </nav>
             </div>
             {
-                account.data && <div className="flex justify-end pt-1 sm:px-6 lg:px-8">
+                account.data && !pathname.includes("/marketplace") && <div className="flex justify-end pt-1 sm:px-6 lg:px-8">
                     <div className="text-white bg-indigo-600 rounded-md p-2">
                         {account.data}
                     </div>
