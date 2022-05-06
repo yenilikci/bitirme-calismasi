7d1e5099c5d053e63d41fee6605b5ba088fa2430
Ceyda <ceydaonemli2@gmail.com>
2022-05-06 23:41:28 +0300

    modified web3 hooks structure

---
+++ components/providers/hooks/web3/useAccount.js
@@ -0,0 +1,5 @@
+import {useHooks} from "@components/providers/web3";
+
+export const useAccount = () => {
+    return useHooks((hooks) => hooks.useAccount)();
+}
--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import {useWeb3} from "@components/providers";
 import {Button} from "@components/ui/common";
-import {useAccount} from "@components/web3/hooks/useAccount";
+import {useAccount} from "@components/providers/hooks/web3/useAccount";
 
 export default function Navbar() {
     const {connect, isLoading, isWeb3Loaded} = useWeb3();
