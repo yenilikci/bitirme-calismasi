ef91f7b45e75da8c3290ba7a03e6af3cfc72707b
Ceyda <ceydaonemli2@gmail.com>
2022-05-08 23:13:41 +0300

    modified course card style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,21 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      maxWidth: {
-        "8xl": "1920px",
+      flex: {
+        "2": "2 2 0%"
       },
+      maxWidth: {
+        "8xl": "1920px"
+      },
+    },
+  },
+  variants: {
+    extend: {
       opacity: ["disabled"],
       cursor: ["disabled"],
     },
   },
   plugins: [],
-};
+}
