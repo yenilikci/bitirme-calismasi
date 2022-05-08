9e3a207e15331e23737816193df5e11fe7b32658
Ceyda <ceydaonemli2@gmail.com>
2022-05-08 23:11:24 +0300

    use require metamask install in navbar

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -4,8 +4,8 @@ import {Button} from "@components/ui/common"
 import {useAccount} from "@components/hooks/web3"
 import {useRouter} from "next/router"
 
-export default function Footer() {
-    const {connect, isLoading, web3} = useWeb3()
+export default function Navbar() {
+    const {connect, isLoading, requireInstall} = useWeb3()
     const {account} = useAccount()
     const {pathname} = useRouter()
 
@@ -47,21 +47,21 @@ export default function Footer() {
                                     onClick={connect}>
                                     Loading...
                                 </Button> :
-                                web3 != null ?
-                                    account.data ?
+                                account.data ?
+                                    <Button
+                                        hoverable={false}
+                                        className="cursor-default">
+                                        Hi there {account.isAdmin && "Admin"}
+                                    </Button> :
+                                    requireInstall ?
                                         <Button
-                                            hoverable={false}
-                                            className="cursor-default">
-                                            Hi there {account.isAdmin && "Admin"}
+                                            onClick={() => window.open("https://metamask.io/download.html", "_blank")}>
+                                            Install Metamask
                                         </Button> :
                                         <Button
                                             onClick={connect}>
                                             Connect
-                                        </Button> :
-                                    <Button
-                                        onClick={() => window.open("https://metamask.io/download.html", "_blank")}>
-                                        Install Metamask
-                                    </Button>
+                                        </Button>
                             }
                         </div>
                     </div>
