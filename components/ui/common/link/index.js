650f59d264771988caca02a5135d63bcfbc2ead9
hilaalyildiz <75807952+hilaalyildiz@users.noreply.github.com>
2022-05-08 23:38:51 +0300

    improved active link

--- components/ui/common/link/index.js
+++ components/ui/common/link/index.js
@@ -1,9 +1,13 @@
 import Link from "next/link"
 import React from "react"
+import {useRouter} from "next/router"
 
-export default function ActiveLink({children, ...props}) {
-
+export default function ActiveLink({children, activeLinkClass, ...props}) {
+    const {pathname} = useRouter()
     let className = children.props.className || ""
+    if (pathname === props.href) {
+        className = `${className} ${activeLinkClass ? activeLinkClass : "text-green-600"}`
+    }
 
     return (
         <Link {...props}>
--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -1,13 +1,13 @@
-import {useWeb3} from "@components/providers"
+import { useWeb3 } from "@components/providers"
 import Link from "next/link"
-import {Button} from "@components/ui/common"
-import {useAccount} from "@components/hooks/web3"
-import {useRouter} from "next/router"
+import { ActiveLink, Button } from "@components/ui/common"
+import { useAccount } from "@components/hooks/web3"
+import { useRouter } from "next/router"
 
 export default function Navbar() {
-    const {connect, isLoading, requireInstall} = useWeb3()
-    const {account} = useAccount()
-    const {pathname} = useRouter()
+    const { connect, isLoading, requireInstall } = useWeb3()
+    const { account } = useAccount()
+    const { pathname } = useRouter()
 
     return (
         <section>
@@ -15,33 +15,33 @@ export default function Navbar() {
                 <nav className="relative" aria-label="Global">
                     <div className="flex justify-between items-center">
                         <div>
-                            <Link href="/">
+                            <ActiveLink href="/" >
                                 <a
                                     className="font-medium mr-8 text-gray-500 hover:text-gray-900">
                                     Home
                                 </a>
-                            </Link>
-                            <Link href="/marketplace">
+                            </ActiveLink>
+                            <ActiveLink href="/marketplace" >
                                 <a
                                     className="font-medium mr-8 text-gray-500 hover:text-gray-900">
                                     Marketplace
                                 </a>
-                            </Link>
-                            <Link href="/">
+                            </ActiveLink>
+                            <ActiveLink href="/blogs" >
                                 <a
                                     className="font-medium mr-8 text-gray-500 hover:text-gray-900">
                                     Blogs
                                 </a>
-                            </Link>
+                            </ActiveLink>
                         </div>
                         <div>
-                            <Link href="/">
+                            <ActiveLink href="/wishlist" >
                                 <a
                                     className="font-medium mr-8 text-gray-500 hover:text-gray-900">
                                     Wishlist
                                 </a>
-                            </Link>
-                            {isLoading ?
+                            </ActiveLink>
+                            { isLoading ?
                                 <Button
                                     disabled={true}
                                     onClick={connect}>
@@ -67,7 +67,7 @@ export default function Navbar() {
                     </div>
                 </nav>
             </div>
-            {account.data &&
+            { account.data &&
             !pathname.includes("/marketplace") &&
             <div className="flex justify-end pt-1 sm:px-6 lg:px-8">
                 <div className="text-white bg-indigo-600 rounded-md p-2">
