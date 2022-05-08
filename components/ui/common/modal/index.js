03e3f2d842c2fee7421c68c21d05684bf0304a38
Ceyda <ceydaonemli2@gmail.com>
2022-05-08 23:17:09 +0300

    modified order modal content

--- components/ui/common/modal/index.js
+++ components/ui/common/modal/index.js
@@ -6,7 +6,8 @@ export default function Modal({isOpen, children}) {
                  role="dialog" aria-modal="true">
                 <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
                     {isOpen &&
-                    <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" ariaHidden="true"></div>
+                    <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+                         aria-hidden="true"></div>
                     }
                     <span className="hidden sm:inline-block sm:align-middle sm:h-screen"
                           aria-hidden="true">&#8203;</span>
