166d97075ebdcf5d0bd75641d52cd62e3f19819d
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-05-08 23:44:25 +0300

    get eth price for marketplace

--- components/hooks/useEthPrice.js
+++ components/hooks/useEthPrice.js
@@ -5,8 +5,15 @@ const URL = "https://api.coingecko.com/api/v3/coins/ethereum?localization=false&
 const fetcher = async url => {
     const res = await fetch(url)
     const json = await res.json()
-    return json
+    return json.market_data.current_price.usd ?? null
 }
 
-export const useEthPrice = () =>
-    useSWR(URL, fetcher);
+export const useEthPrice = () => {
+    const swrRes = useSWR(
+        URL,
+        fetcher,
+        {refreshInterval: 1000}
+    )
+
+    return {eth: {...swrRes}}
+}
