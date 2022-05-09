fe0f8bef156b7c9cb6ab9b3ceee6154f1142ec50
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-05-09 23:56:34 +0300

    improved courses pages

--- pages/marketplace/courses/manage.js
+++ pages/marketplace/courses/manage.js
@@ -1,16 +1,30 @@
-import { OwnedCourseCard } from "@components/ui/course";
-import { BaseLayout } from "@components/ui/layout";
-import { MarketHeader } from "@components/ui/marketplace";
+import {Button} from "@components/ui/common";
+import {CourseFilter, OwnedCourseCard} from "@components/ui/course";
+import {BaseLayout} from "@components/ui/layout";
+import {MarketHeader} from "@components/ui/marketplace";
 
 export default function ManageCourses() {
 
     return (
         <>
             <div className="py-4">
-                <MarketHeader />
+                <MarketHeader/>
+                <CourseFilter/>
             </div>
             <section className="grid grid-cols-1">
-                <OwnedCourseCard />
+                <OwnedCourseCard>
+                    <div className="flex mr-2 relative rounded-md">
+                        <input
+                            type="text"
+                            name="account"
+                            id="account"
+                            className="w-96 focus:ring-indigo-500 shadow-md focus:border-indigo-500 block pl-7 p-4 sm:text-sm border-gray-300 rounded-md"
+                            placeholder="0x2341ab..."/>
+                        <Button>
+                            Verify
+                        </Button>
+                    </div>
+                </OwnedCourseCard>
             </section>
         </>
     )
--- pages/marketplace/courses/owned.js
+++ pages/marketplace/courses/owned.js
@@ -1,16 +1,24 @@
-import { OwnedCourseCard } from "@components/ui/course";
-import { BaseLayout } from "@components/ui/layout";
-import { MarketHeader } from "@components/ui/marketplace";
+import {Button, Message} from "@components/ui/common";
+import {OwnedCourseCard} from "@components/ui/course";
+import {BaseLayout} from "@components/ui/layout";
+import {MarketHeader} from "@components/ui/marketplace";
 
 export default function OwnedCourses() {
 
     return (
         <>
             <div className="py-4">
-                <MarketHeader />
+                <MarketHeader/>
             </div>
             <section className="grid grid-cols-1">
-                <OwnedCourseCard />
+                <OwnedCourseCard>
+                    <Message>
+                        My custom message!
+                    </Message>
+                    <Button>
+                        Watch the course
+                    </Button>
+                </OwnedCourseCard>
             </section>
         </>
     )
