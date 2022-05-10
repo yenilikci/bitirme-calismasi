d8156f6cc52130c293d505f4382ee44c5f72d16a
Ceyda <ceydaonemli2@gmail.com>
2022-05-10 22:18:42 +0300

    load contract artifact

--- components/ui/layout/base/index.js
+++ components/ui/layout/base/index.js
@@ -1,17 +1,16 @@
-import {Footer, Navbar} from "@components/ui/common";
-import {Web3Provider} from "@components/providers";
+import { Web3Provider } from "@components/providers"
+import { Navbar, Footer } from "@components/ui/common"
 
 export default function BaseLayout({children}) {
     return (
         <Web3Provider>
             <div className="max-w-7xl mx-auto px-4">
-                <Navbar/>
+                <Navbar />
                 <div className="fit">
                     {children}
                 </div>
             </div>
-            <Footer/>
+            <Footer />
         </Web3Provider>
     )
 }
-
--- utils/loadContract.js
+++ utils/loadContract.js
@@ -1,18 +1,18 @@
-import contract from "@truffle/contract"
+const NETWORK_ID = process.env.NEXT_PUBLIC_NETWORK_ID
 
-export const loadContract = async (name, provider) => {
+export const loadContract = async (name, web3) => {
     const res = await fetch(`/contracts/${name}.json`)
     const Artifact = await res.json()
-
-    const _contract = contract(Artifact)
-    _contract.setProvider(provider)
-
-    let deployedContract = null
+    let contract = null
+    console.log(Artifact)
     try {
-        deployedContract = await _contract.deployed()
+        contract = new web3.eth.Contract(
+            Artifact.abi,
+            Artifact.networks[NETWORK_ID].address
+        )
     } catch {
         console.log(`Contract ${name} cannot be loaded`)
     }
 
-    return deployedContract
+    return contract
 }
