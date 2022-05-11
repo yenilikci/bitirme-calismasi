ffdc5c4535a92262d3912c82050bd0290a3ad6bc
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-05-11 23:55:49 +0300

    owned courses, web3/ownedCourses

--- components/providers/web3/hooks/useOwnedCourses.js
+++ components/providers/web3/hooks/useOwnedCourses.js
@@ -1,3 +1,22 @@
-export const handler = (web3, contract) => () => {
-    return "UseOwnedCourses is working!"
+import useSWR from "swr"
+
+
+
+export const handler = (web3, contract) => (courses, account) => {
+
+    const swrRes = useSWR(() =>
+        (web3 && contract && account) ? "web3/ownedCourses" : null,
+        async () => {
+            const ownedCourses = []
+
+            for (let i = 0; i < courses.length; i++) {
+                const course = courses[i]
+                ownedCourses.push(course.id)
+            }
+
+            return ownedCourses
+        }
+    )
+
+    return swrRes
 }
