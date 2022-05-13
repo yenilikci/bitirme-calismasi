ca473dc9379092bacb3edafa3df76f0ce245869b
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-05-13 23:53:56 +0300

    wallet bar improvements

--- pages/marketplace/courses/manage.js
+++ pages/marketplace/courses/manage.js
@@ -10,19 +10,7 @@ export default function ManageCourses() {
             <MarketHeader />
             <CourseFilter />
             <section className="grid grid-cols-1">
-                <OwnedCourseCard>
-                    <div className="flex mr-2 relative rounded-md">
-                        <input
-                            type="text"
-                            name="account"
-                            id="account"
-                            className="w-96 focus:ring-indigo-500 shadow-md focus:border-indigo-500 block pl-7 p-4 sm:text-sm border-gray-300 rounded-md"
-                            placeholder="0x2341ab..." />
-                        <Button>
-                            Verify
-                        </Button>
-                    </div>
-                </OwnedCourseCard>
+
             </section>
         </>
     )
