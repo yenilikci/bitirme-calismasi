60777fa87818d27e4d83c9d5550a80a6b8cdcb70
Ceyda <ceydaonemli2@gmail.com>
2022-05-15 02:33:29 +0300

    check owner course badge

--- pages/courses/[slug].js
+++ pages/courses/[slug].js
@@ -1,22 +1,18 @@
-import { useAccount, useOwnedCourse } from "@components/hooks/web3";
-import { Modal } from "@components/ui/common";
-import {
-    CourseHero,
-    Curriculum,
-    Keypoints
-} from "@components/ui/course";
-import { BaseLayout } from "@components/ui/layout";
-import { getAllCourses } from "@content/courses/fetcher";
+import {useAccount, useOwnedCourse} from "@components/hooks/web3";
+import {Modal} from "@components/ui/common";
+import {CourseHero, Curriculum, Keypoints} from "@components/ui/course";
+import {BaseLayout} from "@components/ui/layout";
+import {getAllCourses} from "@content/courses/fetcher";
 
 export default function Course({course}) {
-    const { account } = useAccount()
-    const { ownedCourse } = useOwnedCourse(course, account.data)
-    console.log(ownedCourse)
+    const {account} = useAccount()
+    const {ownedCourse} = useOwnedCourse(course, account.data)
 
     return (
         <>
             <div className="py-4">
                 <CourseHero
+                    hasOwner={!!ownedCourse.data}
                     title={course.title}
                     description={course.description}
                     image={course.coverImage}
