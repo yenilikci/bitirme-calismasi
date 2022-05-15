264e35de92e14e561d0704b69c1afdf79c197f50
hilaalyildiz <75807952+hilaalyildiz@users.noreply.github.com>
2022-05-15 10:48:23 +0300

    implemented useManagedCourses manage page, setup hooks

--- components/providers/web3/hooks/setupHooks.js
+++ components/providers/web3/hooks/setupHooks.js
@@ -1,7 +1,8 @@
-import { handler as createAccountHook } from "./useAccount"
-import { handler as createNetworkHook } from "./useNetwork"
-import { handler as createOwnedCoursesHook } from "./useOwnedCourses"
-import { handler as createOwnedCourseHook } from "./useOwnedCourse"
+import {handler as createAccountHook} from "./useAccount"
+import {handler as createNetworkHook} from "./useNetwork"
+import {handler as createOwnedCoursesHook} from "./useOwnedCourses"
+import {handler as createOwnedCourseHook} from "./useOwnedCourse"
+import {handler as createManagedCoursesHook} from "./useManagedCourses"
 
 
 export const setupHooks = ({web3, provider, contract}) => {
@@ -9,6 +10,7 @@ export const setupHooks = ({web3, provider, contract}) => {
         useAccount: createAccountHook(web3, provider),
         useNetwork: createNetworkHook(web3, provider),
         useOwnedCourses: createOwnedCoursesHook(web3, contract),
-        useOwnedCourse: createOwnedCourseHook(web3, contract)
+        useOwnedCourse: createOwnedCourseHook(web3, contract),
+        useManagedCourses: createManagedCoursesHook(web3, contract),
     }
 }
