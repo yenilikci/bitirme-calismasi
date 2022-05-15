6f8ec417cdcbd42c1e4d34e044ffb0268fd6d207
hilaalyildiz <75807952+hilaalyildiz@users.noreply.github.com>
2022-05-15 10:50:52 +0300

    implemented managed card values

--- components/ui/marketplace/header/index.js
+++ components/ui/marketplace/header/index.js
@@ -8,7 +8,7 @@ const LINKS = [{
     href: "/marketplace/courses/owned",
     value: "My Courses"
 }, {
-    href: "/marketplace/courses/manage",
+    href: "/marketplace/courses/managed",
     value: "Manage Courses"
 }]
 
