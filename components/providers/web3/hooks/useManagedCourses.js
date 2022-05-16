84b0ae52f5738fd04b923814cd9e0783c76776ac
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-05-16 22:59:10 +0300

    add disable managed courses, admin check

--- components/providers/web3/hooks/useManagedCourses.js
+++ components/providers/web3/hooks/useManagedCourses.js
@@ -1,10 +1,12 @@
-import {normalizeOwnedCourse} from "@utils/normalize"
+import { normalizeOwnedCourse } from "@utils/normalize"
 import useSWR from "swr"
 
 export const handler = (web3, contract) => account => {
 
     const swrRes = useSWR(() =>
-            (web3 && contract && account) ? `web3/managedCourses/${account}` : null,
+        (web3 &&
+            contract &&
+            account.data && account.isAdmin) ? `web3/managedCourses/${account.data}` : null,
         async () => {
             const courses = []
             const courseCount = await contract.methods.getCourseCount().call()
@@ -14,7 +16,7 @@ export const handler = (web3, contract) => account => {
                 const course = await contract.methods.getCourseByHash(courseHash).call()
 
                 if (course) {
-                    const normalized = normalizeOwnedCourse(web3)({hash: courseHash}, course)
+                    const normalized = normalizeOwnedCourse(web3)({ hash: courseHash }, course)
                     courses.push(normalized)
                 }
             }
