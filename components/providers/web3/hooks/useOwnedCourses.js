3874d29e60297a51f95f25d5f1b35c3e72066304
Ceyda <ceydaonemli2@gmail.com>
2022-05-18 17:51:29 +0300

    add handle switch networks

--- components/providers/web3/hooks/useOwnedCourses.js
+++ components/providers/web3/hooks/useOwnedCourses.js
@@ -1,18 +1,20 @@
-import { createCourseHash } from "@utils/hash"
-import { normalizeOwnedCourse } from "@utils/normalize"
+import {createCourseHash} from "@utils/hash"
+import {normalizeOwnedCourse} from "@utils/normalize"
 import useSWR from "swr"
 
 export const handler = (web3, contract) => (courses, account) => {
-
     const swrRes = useSWR(() =>
-            (web3 && contract && account) ? `web3/ownedCourses/${account}` : null,
+            (web3 &&
+                contract &&
+                account) ? `web3/ownedCourses/${account}` : null,
         async () => {
             const ownedCourses = []
-
             for (let i = 0; i < courses.length; i++) {
                 const course = courses[i]
 
-                if (!course.id) { continue }
+                if (!course.id) {
+                    continue
+                }
 
                 const courseHash = createCourseHash(web3)(course.id, account)
                 const ownedCourse = await contract.methods.getCourseByHash(courseHash).call()
