0bcdff39ab0963fc47d583472b2fd086ef7c2929
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-05-19 01:37:55 +0300

    add course in progress button implemented

--- components/ui/order/modal/index.js
+++ components/ui/order/modal/index.js
@@ -178,7 +178,7 @@ export default function OrderModal({course, onClose, onSubmit, isNewPurchase}) {
                     <Button
                         disabled={formState.isDisabled}
                         onClick={() => {
-                            onSubmit(order)
+                            onSubmit(order, course)
                         }}>
                         Submit
                     </Button>
