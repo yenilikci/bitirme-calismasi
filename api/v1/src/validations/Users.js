98e8fabda583a67f22f9a6392b654f9cd8bc0889
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-05-19 02:35:24 +0300

    add api users validation with joi

---
+++ api/v1/src/validations/Users.js
@@ -0,0 +1,33 @@
+const Joi = require("joi");
+
+const createValidation = Joi.object({
+    full_name: Joi.string().required().min(3),
+    password: Joi.string().required().min(8),
+    email: Joi.string().email().required().min(8),
+});
+
+const loginValidation = Joi.object({
+    password: Joi.string().required().min(8),
+    email: Joi.string().email().required().min(8),
+});
+
+const resetPasswordValidation = Joi.object({
+    email: Joi.string().email().required().min(8),
+});
+
+const changePasswordValidation = Joi.object({
+    password: Joi.string().required().min(8),
+});
+
+const updateValidation = Joi.object({
+    full_name: Joi.string().min(3),
+    email: Joi.string().email().min(8),
+});
+
+module.exports = {
+    createValidation,
+    loginValidation,
+    resetPasswordValidation,
+    updateValidation,
+    changePasswordValidation,
+};
