5c34258f7ecaf866e239163be3f7eb5ed43cdbba
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-05-19 02:27:54 +0300

    add api loaders , db

---
+++ api/v1/src/loaders/db.js
@@ -0,0 +1,21 @@
+const Mongoose = require("mongoose");
+
+const db = Mongoose.connection;
+
+db.once("open", () => {
+    console.log("Connected to MongoDB");
+});
+
+const connectDB = async () => {
+    await Mongoose.connect(
+        `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    );
+};
+
+module.exports = {
+    connectDB,
+};
