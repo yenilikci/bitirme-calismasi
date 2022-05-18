79a099b050056cee4e0e7e19f4fa4752e44e5fb7
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-05-18 23:32:10 +0300

    add truffle ropsten config

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,5 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
 module.exports = {
     contracts_build_directory: "./public/contracts",
     networks: {
@@ -6,6 +8,21 @@ module.exports = {
             port: 7545,
             network_id: "*",
         },
+        ropsten: {
+            provider: () =>
+                new HDWalletProvider({
+                    mnemonic: {
+                        phrase: ""
+                    },
+                    providerOrUrl: "https://ropsten.infura.io/v3/YOUR-PROJECT-ID",
+                    addressIndex: 0
+                }),
+            network_id: 3,
+            gas: 5500000, // Gas Limit, How much gas we are willing to spent
+            gasPrice: 20000000000, // how much we are willing to spent for unit of gas
+            confirmations: 2, // number of blocks to wait between deployment
+            timeoutBlocks: 200 // number of blocks before deployment times out
+        }
     },
     compilers: {
         solc: {
