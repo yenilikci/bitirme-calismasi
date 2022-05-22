18657190b520ef28e9bcbe2b16c55721866425a7
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-05-22 11:09:40 +0300

    add api setting controller

---
+++ api/v1/src/controllers/Settings.js
@@ -0,0 +1,34 @@
+const {
+    insert,
+    modify,
+} = require("../services/Settings");
+const httpStatus = require("http-status");
+const uuid = require("uuid");
+const eventEmitter = require("../scripts/events/eventEmitter");
+
+const index = (req, res) => {
+    list()
+        .then((response) => {
+            res.status(httpStatus.OK).send(response);
+        })
+        .catch((e) => {
+            res.status(httpStatus.INTERNAL_SERVER_ERROR).send(e);
+        });
+};
+
+const update = (req, res) => {
+    modify({_id: req.setting?._id}, req.body)
+        .then((updatedSetting) => {
+            res.status(httpStatus.OK).send(updatedSetting);
+        })
+        .catch(() => {
+            res
+                .status(httpStatus.INTERNAL_SERVER_ERROR)
+                .send({error: "Failed setting update"});
+        });
+};
+
+module.exports = {
+    index,
+    update
+};
